25f0d0a220dfff29a6a428b1c988548cc3079f7e
Munavvar <munavvarspace@gmail.com>
2022-12-05 02:50:23 +0300

    тройка Пифагора

--- jaj.js
+++ jaj.js
@@ -3,39 +3,56 @@ const dlinaB = document.getElementById('b')
 const dlinaC = document.getElementById('c')
 const buttonResult = document.querySelector('.bresult')
 const result = document.querySelector('.result')
-// 
+ 
 
 console.log('f')
-// let a = dlinaA.value
-// let b = dlinaB.value
-// console.log(a)
-// console.log(b)
 
+
+// Number(dlinaA.value)
+// Number(dlinaB.value)
+// Number(dlinaC.value)
 function SP() {
-    let a = Number(dlinaA.value)
-    let b = Number(dlinaB.value)
-    let c = Number(dlinaC.value)
-    console.log(a)
-    console.log(b)
-    let x1
-    let x2
+    function Ra() {
+        
+        if (a*a === (b*b + c*c)) {
+            result.innerHTML = `эти цифры тройка пифагора <br/> ${a} самое большое число`
+        } else {
+            console.log(a)
+            result.innerHTML = `это не тройка Пифагора`
+            }
+        }
+    let a 
+    let b
+    let c
+    let d = true
+    if ((dlinaA.value == dlinaB.value) || (dlinaC.value == dlinaA.value) || (dlinaB.value == dlinaC.value)) {
+        result.innerHTML = `некоторые числа равны`
+    } else {
+        switch (d) {
+            case Number(dlinaA.value) > Number(dlinaB.value) && Number(dlinaA.value) > Number(dlinaC.value):
+                a = Number(dlinaA.value)
+                b = Number(dlinaB.value)
+                c = Number(dlinaC.value)
+                break;
+            case Number(dlinaB.value) > Number(dlinaA.value) && Number(dlinaB.value) > Number(dlinaC.value):
+                a = Number(dlinaB.value)
+                b = Number(dlinaA.value)
+                c = Number(dlinaC.value)
+                break;
+            case Number(dlinaC.value) > Number(dlinaA.value) && Number(dlinaC.value) > Number(dlinaB.value):
+                a = Number(dlinaC.value)
+                b = Number(dlinaA.value)
+                c = Number(dlinaB.value)
+                break;
+            default:
+                result.innerHTML = `нету единственного большого числа`
+                break;
+        }
+        Ra()
+           
+    }
     
-    const d = b*b - 4*a*c
-    console.log(d)
-    if (d>0) {
-        console.log(d)
-        x1 = ((-b + Math.sqrt(d))/(2*a))
-        console.log(x1)
-        x2 = ((-b - Math.sqrt(d))/(2*a))
-        result.innerHTML = `${Math.round(x1)} -первый корень <br/> ${Math.round(x2)} -второй корень`
-    } 
-    if (d == 0) {
-        x1 = -b/(2*a)
-        result.innerHTML = `уровнение имеет 1 корень ${x1}`
-    }
-    if (d<0) {
-        result.innerHTML = `уравнение неправильное`
-    }
+
 }
 
 buttonResult.addEventListener('click', SP);
