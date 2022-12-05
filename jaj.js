a74886d40079051441edf6e041bea1eded50fb29
Munavvar <munavvarspace@gmail.com>
2022-12-05 17:51:59 +0300

    количество и список делителей

--- jaj.js
+++ jaj.js
@@ -12,47 +12,17 @@ console.log('f')
 // Number(dlinaB.value)
 // Number(dlinaC.value)
 function SP() {
-    function Ra() {
-        
-        if (a*a === (b*b + c*c)) {
-            result.innerHTML = `эти цифры тройка пифагора <br/> ${a} самое большое число`
-        } else {
-            console.log(a)
-            result.innerHTML = `это не тройка Пифагора`
-            }
+    let a = Number(dlinaA.value)
+    const massiv = []
+    for (let i = 1; i <= a; i++) {
+        let b = a/i
+        if (Number.isInteger(b)) {
+            massiv.push(b)
+            
         }
-    let a 
-    let b
-    let c
-    let d = true
-    if ((dlinaA.value == dlinaB.value) || (dlinaC.value == dlinaA.value) || (dlinaB.value == dlinaC.value)) {
-        result.innerHTML = `некоторые числа равны`
-    } else {
-        switch (d) {
-            case Number(dlinaA.value) > Number(dlinaB.value) && Number(dlinaA.value) > Number(dlinaC.value):
-                a = Number(dlinaA.value)
-                b = Number(dlinaB.value)
-                c = Number(dlinaC.value)
-                break;
-            case Number(dlinaB.value) > Number(dlinaA.value) && Number(dlinaB.value) > Number(dlinaC.value):
-                a = Number(dlinaB.value)
-                b = Number(dlinaA.value)
-                c = Number(dlinaC.value)
-                break;
-            case Number(dlinaC.value) > Number(dlinaA.value) && Number(dlinaC.value) > Number(dlinaB.value):
-                a = Number(dlinaC.value)
-                b = Number(dlinaA.value)
-                c = Number(dlinaB.value)
-                break;
-            default:
-                result.innerHTML = `нету единственного большого числа`
-                break;
-        }
-        Ra()
-           
+    result.innerHTML = `делителей - ${massiv.length} <br/> делители ${massiv}`
     }
     
-
 }
 
 buttonResult.addEventListener('click', SP);
