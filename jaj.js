f229631dbb7c237186bd3ae1e8758b5cfe25d48c
Munavvar <munavvarspace@gmail.com>
2022-12-08 00:07:43 +0300

    количество слов

--- jaj.js
+++ jaj.js
@@ -1,56 +1,20 @@
-const dlinaA = document.getElementById('a')
-const dlinaB = document.getElementById('b')
-const dlinaC = document.getElementById('c')
-const buttonResult = document.querySelector('.bresult')
+const ta = document.querySelector('#ta')
 const result = document.querySelector('.result')
- 
 
-console.log('f')
+let massiv = []
 
 
-// Number(dlinaA.value)
-// Number(dlinaB.value)
-// Number(dlinaC.value)
-function SP() {
-    let a = Number(dlinaA.value)
-    let b = Number(dlinaB.value)
-    let c
-    let d
-    const massiv = []
-    let key = true
-    switch (key) {
-        case a > b && b > 0:
-            c = -b
-            d = a
-            break;
-        case a > b && b < 0:
-            c = b
-            d = a
-            break;
-        case a < b && a > 0:
-            c = -a
-            d = b
-            break;
-        case a < b && a < 0:
-            c = a
-            d = b
-            break;
-        default:
-            c = a
-            d = b
-            break;
-    }
-    console.log(c)
-    console.log(d)
-    for (let i = c; i <= d; i++) {
-        delitelA = c/i
-        delitelB = d/i
-        if (Number.isInteger(delitelA) && Number.isInteger(delitelB)) {
-            massiv.push(i)
-        }
-    result.innerHTML = `общих делителей - ${massiv.length} <br/> самый маленький делитель - ${Math.min.apply(null, massiv)}` //Math.max(...massiv)
-    }
-
+function blur() {
+    massiv = ta.value.split(' ')
+    massiv = massiv.filter((el) => el != '')   
+    console.log(massiv)
+    result.innerHTML = `количество слов ${massiv.length}`
 }
 
-buttonResult.addEventListener('click', SP);
+function focus() {
+    result.innerHTML = ``
+}
+
+
+ta.addEventListener('blur', blur)
+ta.addEventListener('focus', focus)
